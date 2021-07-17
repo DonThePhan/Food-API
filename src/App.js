cf0e669bfd2edd2cdab4e5ffaea68a46acefa6ee
Donny <donthephan@gmail.com>
2021-07-16 23:56:38 -0400

    Food Search Complete

--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@ import './App.css';
 import MainHeader from './components/nav/MainHeader';
 import Layout from './components/layout/Layout';
 import { Fragment } from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import SearchRecipes from './pages/SearchRecipes';
 import FoodDB from './pages/FoodDB';
-import { Route, Redirect } from 'react-router-dom';
+import RecipeDetails from './pages/RecipeDetails';
 
 function App() {
 	return (
 		<Fragment>
 			<MainHeader />
 			<Layout>
-				<Route path="/" exact>
-					<Redirect to="/search-recipes" />
-				</Route>
-				<Route path="/search-recipes">
-					<SearchRecipes />
-				</Route>
-				<Route path="/fooddb">
-					<FoodDB />
-				</Route>
+				<Switch>
+					<Route path={`/search-recipes/:recipeId`} exact>
+						<RecipeDetails />
+					</Route>
+					<Route path="/" exact>
+						<Redirect to="/search-recipes" />
+					</Route>
+					<Route path="/search-recipes" exact>
+						<SearchRecipes />
+					</Route>
+					<Route path="/fooddb" exact>
+						<FoodDB />
+					</Route>{' '}
+				</Switch>
 			</Layout>
 		</Fragment>
 	);
